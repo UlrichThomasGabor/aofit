035a0d521fd10e9344af59645badb48a19d21dc4
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2018-02-23 15:34:26 +0100

    Prettified Python code.

--- scripts/patch.py
+++ scripts/patch.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-
 import sys
 import subprocess
 import re
 
 binaryPath = 'build/kratos.elf'
-configSymbol = 'fi_config_arr' #name of config array, set in config aspect
+configSymbol = 'fi_config_arr' # name of config array, set in config aspect
 
-objdump_output = subprocess.run(['objdump', '-D', '--section=.data', '--file-offsets', binaryPath], stdout=subprocess.PIPE).stdout.decode(encoding = sys.getdefaultencoding())
+objdump_output = subprocess.run(['objdump', '-D', '--section=.data', '--file-offsets', binaryPath], stdout=subprocess.PIPE).stdout.decode(encoding=sys.getdefaultencoding())
 
-ln = re.findall('<'+configSymbol+'>.*\n', objdump_output) #find line about config array
+ln = re.findall('<'+configSymbol+'>.*\n', objdump_output) # find line about config array
 print('line: ', ln)
-if(len(ln)>0):
-    offset = re.findall(r'0x[0-9a-f]+', ln[0]) #extract offset of config array (hex value)
+if(len(ln) > 0):
+    offset = re.findall(r'0x[0-9a-f]+', ln[0]) # extract offset of config array (hex value)
     print('offset: ', offset)
 
 binary = open(binaryPath, 'r+b')
-offset = int(offset[0], base=16)
+offset = int(offset[0], base = 16)
 binary.seek(offset)
-binary.write(b'$BCDEFGHIJKLMNO$') #write configuration data as bytes
+binary.write(b'$BCDEFGHIJKLMNO$') # write configuration data as bytes
 binary.close()
--- scripts/serial.py
+++ scripts/serial.py
@@ -14,29 +14,28 @@ magicStr = bytearray("e84fc95d3a41b3ebfccedea78311657c", encoding='ascii')
 pos = 0
 imagePath = 'build/bootdisk.vmi'
 
-#start qemu with built OS concurrently and redirect serial output to opened pts
+# start qemu with built OS concurrently and redirect serial output to opened pts
 proc = subprocess.Popen(['qemu-system-x86_64', '-chardev', 'tty,id=pts'+slave_id+',path=/dev/pts/'+slave_id, '-device', 'isa-serial,chardev=pts'+slave_id, imagePath], stdout=subprocess.PIPE)
 
 while True:
     c = pts.read(1)
-    if c[0]==magicStr[pos]:
-        if pos==len(magicStr)-1 and stop:
+    if c[0] == magicStr[pos]:
+        if pos == len(magicStr) - 1 and stop:
             print("detected final magic string!")
             break
-        elif pos==len(magicStr)-1:
+        elif pos == len(magicStr) - 1:
             print("detected first magic string!")
             stop = True
-            pos=0
-            tmp=b""
+            pos = 0
+            tmp = b""
         pos += 1
         tmp += c #save read data in case it's not part of the magic string
         continue
     else:
-        pos=0
+        pos = 0
         read_input += tmp #tmp from previous iterations
         tmp = b""
         read_input += c
 
 proc.terminate();
-print("output: \n", read_input)    
-    
+print("output: \n", read_input)
