c87c98635884a61c176a00d6e9972b2d5ffe50f6
cordt <cordt.von-egidy@tu-dortmund.de>
2018-03-01 20:01:46 +0100

    patch and serial now usable both from other python scripts and as a standalone script

--- scripts/patch.py
+++ scripts/patch.py
@@ -5,42 +5,79 @@ import re
 import platform
 import argparse
 
-parser = argparse.ArgumentParser(description='Modify an ELF file.')
-parser.add_argument("elf_file", help="The ELF file to modify.", default="build/kratos.elf")
-parser.add_argument("--config_symbol", help="The symbol to modify.", default="fi_config_arr")
-parser.add_argument("--debug", action="store_true", default=False);
-args = parser.parse_args()
+run_as_script = False #change to True if run as script instead of called as an imported function
 
-if platform.system() != "Linux":
-    print("This tool works only on Linux, because only there objdump can print the necessary information.")
-    exit(1)
+def patch_local(binaryPath, configData, configSymbol="fi_config_arr", debug=False):
+    if platform.system() != "Linux":
+        print("This tool works only on Linux, because only there objdump can print the necessary information.")
+        if run_as_script:
+            exit(1)
+        else:
+            return False
 
-binaryPath = args.elf_file
-configSymbol = args.config_symbol # name of config array, set in config aspect
+    print("Searching for symbol " + configSymbol + " in ELF " + binaryPath + ".")
 
-print("Searching for symbol " + configSymbol + " in ELF " + args.elf_file + ".")
+    try:
+        objdump_subprocess = subprocess.run(['objdump', '-D', '--section=.data', '--file-offsets', binaryPath], stdout=subprocess.PIPE, check=True) #only stderr is printed
+    except:
+        #objdump_subprocess can't be accessed: the except block assumes that it wasn't assigned. However, objdump's output is printed
+        print("Objdump failed")
+        if run_as_script:
+            exit(1)
+        else:
+            return False
 
-objdump_subprocess = subprocess.run(['objdump', '-D', '--section=.data', '--file-offsets', args.elf_file], stdout=subprocess.PIPE)
-objdump_output = objdump_subprocess.stdout.decode(encoding=sys.getdefaultencoding())
-if args.debug:
-	print("objdump output:")
-	print(objdump_output)
+    objdump_output = objdump_subprocess.stdout.decode(encoding = sys.getdefaultencoding())
+    if debug:
+        print("Objdump output:")
+        print(objdump_output)
+    ln = re.findall('<' + configSymbol + '>.*\n', objdump_output) # find line about config array
+    if len(ln) == 0:
+        print("Could not find the symbol " + configSymbol + " with configuration information in the ELF file.")
+        if run_as_script:
+            exit(1)
+        else:
+            return False
+    else:
+        if debug:
+            print('Found the symbol on line:', ln)
+        
+        offset = re.findall(r'0x[0-9a-f]+', ln[0]) # extract offset of config array (hex value)
+        if debug:
+            print('Found the offset:', offset)
 
-ln = re.findall('<' + args.config_symbol + '>.*\n', objdump_output) # find line about config array
-if len(ln) == 0:
-	print("Could not find the symbol " + args.config_symbol + " with configuration information in the ELF file.")
-	exit(1)
-else:
-	if args.debug:
-		print('Found the symbol on line: ' + ln)
-	
-	offset = re.findall(r'0x[0-9a-f]+', ln[0]) # extract offset of config array (hex value)
-	if args.debug:
-		print('Found the offset: ' + offset)
+        try:
+            binary = open(binaryPath, 'r+b')
+        except OSError:
+            print("Failed to open binary file", binaryPath)
+            if run_as_script:                
+                exit(1)
+            else:
+                return False
 
-	binary = open(binaryPath, 'r+b')
-	offset = int(offset[0], base=16)
-	binary.seek(offset)
-	binary.write(b'$BCDEFGHIJKLMNO$') # write configuration data as bytes
-	binary.close()
+        offset = int(offset[0], base=16)
+        binary.seek(offset)
+        binary.write(configData) #write configuration data as bytes
+        binary.close()
 
+        print("Successfully patched", binaryPath)
+        if run_as_script:
+            exit(0)
+        else:
+            return True
+
+if __name__ == "__main__":
+    run_as_script = True
+
+    parser = argparse.ArgumentParser(description='Modify an ELF file.')
+    parser.add_argument("elf_file", help="The ELF file to modify.")
+    parser.add_argument("--config_symbol", help="The symbol to modify.", default="fi_config_arr")
+    parser.add_argument("config_data", help="The data to be written to the configuration space")
+    parser.add_argument("--debug", action="store_true", default=False)
+    args = parser.parse_args()
+
+    binaryPath = args.elf_file
+    configSymbol = args.config_symbol # name of config array, set in config aspect
+    configData = bytes(args.config_data, encoding=sys.getdefaultencoding())
+
+    patch_local(binaryPath, configData, configSymbol, args.debug)
--- scripts/serial.py
+++ scripts/serial.py
@@ -1,52 +1,88 @@
 #!/usr/bin/env python3
+
 import pty
 import os
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Start QEMU and bind serial output to openend PTS.')
-parser.add_argument("image", help="The image to start.", default="build/bootdisk.vmi")
-parser.add_argument("--debug", action="store_true", default=False);
-args = parser.parse_args()
+run_as_script = False #change to True if run as script instead of called as an imported function
 
-master, slave = pty.openpty()
-if args.debug:
-	print("master: " + os.ttyname(master) + "\tslave: " + os.ttyname(slave))
-slave_id = os.ttyname(slave).split('/')[-1]
+def launch_serial(image, magicStr, debug=False):
+    master, slave = pty.openpty()
+    if debug:
+        print("master: " + os.ttyname(master) + "\tslave: " + os.ttyname(slave))
+    slave_id = os.ttyname(slave).split('/')[-1]
 
-pts = open(master, 'rb', buffering=0)
+    pts = open(master, 'rb', buffering=0)
 
-read_input = b""
-tmp = b""
-stop = False
-magicStr = bytearray("e84fc95d3a41b3ebfccedea78311657c", encoding='ascii')
-pos = 0
+    read_input = b""
+    tmp = b""
+    stop = False
+    pos = 0
 
-# Start QEMU and redirect serial output to opened pts.
-io_options = ["-chardev", "tty,id=pts" + slave_id + ",path=/dev/pts/" + slave_id, "-device", "isa-serial,chardev=pts" + slave_id]
-image_options = ["-drive", "file=" + args.image + ",index=0,if=floppy,format=raw", "-boot", "a", "-k", "en-us"]
-proc = subprocess.Popen(['qemu-system-x86_64'] + io_options + image_options, stdout=subprocess.PIPE)
+    # Start QEMU and redirect serial output to opened pts.
+    io_options = ["-chardev", "tty,id=pts" + slave_id + ",path=/dev/pts/" + slave_id, "-device", "isa-serial,chardev=pts" + slave_id]
+    image_options = ["-drive", "file=" + image + ",index=0,if=floppy,format=raw", "-boot", "a", "-k", "en-us"]
+    try:
+        proc = subprocess.Popen(['qemu-system-x86_64'] + io_options + image_options, stdout=subprocess.PIPE) # stdout=subprocess.PIPE leads to qemu not terminating
+    except:
+        print("Launching Qemu failed.")
+        if run_as_script:
+            exit(1)
+        else:
+            return None
 
-while True:
-    c = pts.read(1)
-    if c[0] == magicStr[pos]:
-        if pos == len(magicStr) - 1 and stop:
-            print("Detected final magic string!")
-            break
-        elif pos == len(magicStr) - 1:
-            print("Detected first magic string!")
-            stop = True
+    ok=False
+    try:
+        proc.wait(timeout=1) #wait a second for qemu to (possibly) encounter errors
+        #pts buffers 4kb in each direction; further writes block
+        #TODO run read thread parallely as soon as pts is opened, so no data is lost
+    except subprocess.TimeoutExpired: #no termination within last second -> assume everything's ok
+        ok=True
+
+    if not ok and proc.returncode != 0:
+        print("Qemu failed")
+        if run_as_script:
+            exit(1)
+        else:
+            return None
+
+    while True:
+        c = pts.read(1)
+        if c[0] == magicStr[pos]:
+            if pos == len(magicStr) - 1 and stop:
+                if debug:
+                    print("Detected final magic string!")
+                break
+            elif pos == len(magicStr) - 1:
+                if debug:
+                    print("Detected first magic string!")
+                stop = True
+                pos = 0
+                tmp = b""
+            pos += 1
+            tmp += c #save read data in case it's not part of the magic string
+            continue
+        else:
             pos = 0
+            read_input += tmp #tmp from previous iterations
             tmp = b""
-        pos += 1
-        tmp += c #save read data in case it's not part of the magic string
-        continue
+            read_input += c
+
+    proc.terminate();
+    if run_as_script:
+        print("retrieved data:")
+        print(read_input) #TODO dump to file?
     else:
-        pos = 0
-        read_input += tmp #tmp from previous iterations
-        tmp = b""
-        read_input += c
+        return read_input
 
-proc.terminate();
-print("output: \n", read_input)
+if __name__=="__main__":
+    run_as_script = True
 
+    parser = argparse.ArgumentParser(description='Start QEMU and bind serial output to openend PTS.')
+    parser.add_argument("image", help="The image to start.", default="build/bootdisk.vmi")
+    parser.add_argument("magic_string", help="The magic string framing the relevant output")
+    parser.add_argument("--debug", action="store_true", default=False);
+    args = parser.parse_args()
+
+    launch_serial(args.image, bytearray(args.magic_string, encoding='ascii'), args.debug)
